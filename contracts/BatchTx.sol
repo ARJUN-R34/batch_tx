a6b48124099cd113b1638c64777073f831d04d96
Arjun Sasikumar <arjun1709@gmail.com>
2023-01-25 11:36:20 +0530

    Update BatchTx.sol

--- contracts/BatchTx.sol
+++ contracts/BatchTx.sol
@@ -2,26 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract TokenBatchTx {
+contract BatchTokenTransfer {
 
-    using SafeMath for uint256;
-
-    ERC20 public token;
+    ERC20 public erc20;
 
     // mapping to keep track of admins
     mapping (address => bool) public whitelistedUsers;
+
+    constructor(address _token) {
+        erc20 = ERC20(_token);
+        whitelistedUsers[msg.sender] = true;
+    }
 
     // Modifier to check if the function caller is an admin
     modifier onlyAdmin() {
         require(whitelistedUsers[msg.sender] == true, "Only admin can call this function.");
         _;
-    }
-
-    constructor(address _token) {
-        token = ERC20(_token);
-        whitelistedUsers[msg.sender] = true;
     }
 
     // admin function to add a new admin
@@ -33,13 +30,29 @@ contract TokenBatchTx {
         return true;
     }
 
-    // To transfer tokens from Contract to the provided list of token holders with respective amount
-    function batchTransfer(address fromAddress, address[] memory targetAddresses, uint256[] memory amounts) external onlyAdmin {
-        require(targetAddresses.length == amounts.length, "Invalid number of recipients and amounts");
+    // event to emit when a batch transfer is complete
+    event BatchTransfer(address fromAddress, address[] recipients, uint256[] amounts);
 
-        for (uint256 indx = 0; indx < targetAddresses.length; indx++) {
-            require(token.transferFrom(fromAddress, targetAddresses[indx], amounts[indx]), "Unable to transfer token to the account");
+    // sample event to check allowance
+    event Allowance(address spender, address owner, uint256 amount);
+
+    // function to perform batch transfer of tokens
+    function batchTransfer(address fromAddress, address[] memory recipients, uint256[] memory amounts) public onlyAdmin {
+        require(recipients.length == amounts.length, "Invalid input: mismatched number of recipients and amounts.");
+
+        // loop through recipients and amounts to transfer tokens
+        for (uint256 i = 0; i < recipients.length; i++) {
+            // check if the address is a valid Ethereum address
+            require(recipients[i] != address(0), "Invalid input: recipient address is not valid.");
+
+            // check if the amount is greater than 0
+            require(amounts[i] > 0, "Invalid input: transfer amount must be greater than 0.");
+
+            // transfer tokens
+            erc20.transferFrom(fromAddress, recipients[i], amounts[i]);
         }
-    }
 
+        // emit event
+        emit BatchTransfer(fromAddress, recipients, amounts);
+    }
 }
